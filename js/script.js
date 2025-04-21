b12f38a7a25afe2bd104572595e26eead1d2dc09
NaniSmarty <narayanaraju060@gmail.com>
2025-04-21 18:29:51 +0530

    RedGoat

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+
+// carousel section
+
+
+
+// Infinite Scroll Animation
+
+
+
+// Gallery
+
+
+// why us
+  
+// Dom connect function
+
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll('a[href^="#"]');
+
+  links.forEach(link => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      const headerOffset = 80; // Adjust if your navbar is fixed
+      const elementPosition = target.getBoundingClientRect().top;
+      const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth"
+      });
+    });
+  });
+});
